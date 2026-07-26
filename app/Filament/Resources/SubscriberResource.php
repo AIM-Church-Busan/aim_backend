d6871da82128d1b412078c40828143226189358e
unknown <riiachoii@gmail.com>
2026-07-26 23:31:08 +0900

    fix(Subscriber Filmanet Page): from Korean to English

--- app/Filament/Resources/SubscriberResource.php
+++ app/Filament/Resources/SubscriberResource.php
@@ -24,28 +24,28 @@ class SubscriberResource extends Resource
 {
     protected static ?string $model = Subscriber::class;
     protected static ?string $navigationIcon = 'heroicon-o-envelope';
-    protected static ?string $navigationLabel = '뉴스레터 구독자';
+    protected static ?string $navigationLabel = 'Subsribers';
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('email')->searchable(),
-                Tables\Columns\IconColumn::make('is_confirmed')->boolean()->label('확인됨'),
+                Tables\Columns\IconColumn::make('is_confirmed')->boolean()->label('confirmed'),
                 Tables\Columns\TextColumn::make('confirmed_at')->dateTime('Y-m-d H:i'),
                 Tables\Columns\TextColumn::make('unsubscribed_at')->dateTime('Y-m-d H:i'),
             ])
-            ->filters([Tables\Filters\TernaryFilter::make('is_confirmed')->label('확인 여부')])
+            ->filters([Tables\Filters\TernaryFilter::make('is_confirmed')->label('Only Confirmed')])
             ->headerActions([
                 Tables\Actions\Action::make('sendNewsletter')
-                    ->label('뉴스레터 발송')
+                    ->label('Send Newsletter')
                     ->form([
-                        Forms\Components\TextInput::make('subject')->label('제목')->required(),
-                        Forms\Components\RichEditor::make('body')->label('내용')->required(),
+                        Forms\Components\TextInput::make('subject')->label('Subject')->required(),
+                        Forms\Components\RichEditor::make('body')->label('Body')->required(),
                     ])
                     ->action(function (array $data) {
                         DispatchNewsletterJob::dispatch($data['subject'], $data['body']);
-                        Notification::make()->title('발송 큐에 등록되었습니다.')->success()->send();
+                        Notification::make()->title('Your letter has been registered on the queue.')->success()->send();
                     }),
             ])
             ->actions([Tables\Actions\DeleteAction::make()]);
